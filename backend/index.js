c3471967230bc8fffc3c3f56c82c58c55df35ca2
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-08-13 00:42:14 -0500

    v0.0.1 Added index.js, config.js - Added bcryptjs, mongoose and express - Expecting to have X.509 auth certificates working soon

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,18 @@ const express = require("express");
 
 const app = express();
 
+//CORS middleware
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({
     ok: true
   });
 });
 
-app.listen(process.env.PORT, () => {
+//Server start
+app.listen(process.env.PORT || 4000, () => {
   console.log(
     `Bienvenido, versión actual v${process.env.VERSION} \n Servidor corriendo en el puerto: ${process.env.PORT}`
   );
