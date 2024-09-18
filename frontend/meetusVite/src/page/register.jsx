956e4b6cc5421166b04cc94265e728311784bb38
Nicolas Gonzalez Franco <nicolas290804@hotmail.com>
2024-09-18 00:19:53 -0500

    Main Agregue la carpeta page en src para agregar las paginas de login y register este ultimo ya tiene avances

---
+++ frontend/meetusVite/src/page/register.jsx
@@ -0,0 +1,16 @@
+import { Container } from "react-bootstrap";
+
+const register = () => {
+    return (
+    <Container>
+        <div>
+        <h1>sign up Mecgraf</h1>
+        <input type="text" placeholder="Username"></input>
+        <input type="password" placeholder="Password"></input>
+        <p></p>
+        </div>
+    </Container>
+    );
+};
+
+export default register;
