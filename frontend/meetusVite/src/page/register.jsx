acec61905b071cceb6bb29a18dd9dacd0e698970
Etienne <etienne.samboni@uao.edu.co>
2024-09-18 14:34:48 -0500

    Change some minimun errors.

--- frontend/meetusVite/src/page/register.jsx
+++ frontend/meetusVite/src/page/register.jsx
@@ -4,10 +4,9 @@ const register = () => {
     return (
     <Container>
         <div>
-        <h1>sign up Mecgraf</h1>
+        <h1>sign up MeetUs</h1>
         <input type="text" placeholder="Username"></input>
         <input type="password" placeholder="Password"></input>
-        <p></p>
         </div>
     </Container>
     );
