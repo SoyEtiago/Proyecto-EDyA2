12e7b72a21a7f41859b23d0db915489e22af35e5
Nicolas Gonzalez Franco <nicolas290804@hotmail.com>
2024-09-23 23:43:03 -0500

    Front debug No admitir el pul request esta en pruebas.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Register from './page/register.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+
   </StrictMode>,
 )
