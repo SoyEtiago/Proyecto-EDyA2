e750b26795ec0ac99c2436ae912078142b56e36e
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-10-14 21:07:56 -0500

    Added registration route  to the React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './page/HomePage';
 import Login from './page/Login';
+import Register from './page/Register';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login onLogin={() => setIsLoggedIn(true)} />} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
       {isLoggedIn && <h1>Bienvenido, has iniciado sesión correctamente!</h1>}
     </Router>
